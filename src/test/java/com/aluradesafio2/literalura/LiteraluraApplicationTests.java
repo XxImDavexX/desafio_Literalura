891842f691cca0a09c84a55d366896aec93c0955
XxImDavexX <david411dark@gmail.com>
2024-06-21 11:56:54 -0600

    Creacion del projecto con Spring

---
+++ src/test/java/com/aluradesafio2/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluradesafio2.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
